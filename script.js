aae72df78b35417817795adf7abf58a0fe9f4deb
AlexanderLe <a.p.a.le@students.uu.nl>
2024-02-28 15:40:16 +0100

    - Placed images in a folder - Added new links - Updated styling - Made more media queries to be more accessible.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function myFunction() {
-    var x = document.getElementById("myLinks");
+    var x = document.getElementById("navigation");
     if (x.style.display === "block") {
         x.style.display = "none";
     } else {
